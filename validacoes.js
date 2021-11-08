87dd72c441bc02d44037bee979b32cf2df638c26
REBECA CAMARGO DE FARIAS <rebeca.farias@alunoescolassociais.org.br>
2021-11-08 17:24:18 -0300

    última commit

---
+++ validacoes.js
@@ -0,0 +1,16 @@
+function verificar(){
+	var nome = document.geElementById('nome').value;
+	var email = document.geElementById('email').value;
+	var celular = document.geElementById('celular').value;
+	var cpf = document.geElementById('cpf').value;
+	var assunto = document.geElementById('asssunto').value;
+	var detalhes = document.geElementById('detalhes').value;
+	var tipodepessoa = document.getElementById('tipodepessoa').value;
+	var senha = document.geElementById('senha').value;
+	var confirmarsenha = document.geElementById(confirmarsenha)
+
+if (!nome || !email || !celular || !cpf || !cpf || !assunto || !detalhes || !tipodepessoa || !senha || !confirmarsenha)
+}else{
+	alert("Campos preenchidos com sucesso")
+	;}
+	
