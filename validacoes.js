63f2518f65ab0af942c755e98201aa35537caade
REBECA CAMARGO DE FARIAS <rebeca.farias@alunoescolassociais.org.br>
2021-11-22 17:38:23 -0300

    incrementa projeto com validações utilizando JavaScript

--- validacoes.js
+++ validacoes.js
@@ -1,21 +1,25 @@
 function verificar(){
-	var nome = document.geElementById('nome').value;
-	var email = document.geElementById('email').value;
-	var celular = document.geElementById('celular').value;
-	var cpf = document.geElementById('cpf').value;
-	var assunto = document.geElementById('asssunto').value;
-	var detalhes = document.geElementById('detalhes').value;
-	var tipodepessoa = document.getElementById('tipodepessoa').value;
-	var senha = document.geElementById('senha').value;
-	var confirmarsenha = document.geElementById(confirmarsenha)
 
-if (!nome || !email || !celular || !cpf || !cpf || !assunto || !detalhes || !tipodepessoa || !senha || !confirmarsenha)
-{
+	var nome = document.getElementById('nome').value;
+	var cpf = document.getElementById('cpf').value;
+	var email = document.getElementById('email').value;
+	var celular = document.getElementById('celular').value;
+	var assunto = document.getElementById('assunto').value;
+	var detalhes = document.getElementById('detalhes').value;
+	var senha = document.getElementById('senha').value;
+	var confirmarsenha = document.getElementById('confirmarsenha').value;
+	var endereco = document.getElementById('endereco').value;
+	var bairro = document.getElementById('bairro').value;
+	var cidade = document.getElementById('cidade').value;
+	var estado = document.getElementById('estado').value;
+	var pontodereferencia = document.getElementById('pontodereferencia').value;
+
+if (!nome || !email || !cpf || !celular || !assunto || !detalhes || !senha || !confirmarsenha || !endereco || !bairro || !cidade || !estado || !pontodereferencia){
 	alert("Campos não preenchidos, favor preencher.");
 	} else {
-		alert("Campos preenchidos com sucesso")
+		alert("Campos preenchidos com sucesso");
 	}
-
+}
 
 	function maskFone (){
 	const numeroAtual = document.getElementById('celular').value
